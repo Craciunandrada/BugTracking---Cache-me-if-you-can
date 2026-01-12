b9145aaf300c2ac3e5a22aa50a15031041f5b659
teo-007 <dodisiteodora23@stud.ase.ro>
2026-01-12 21:46:11 +0200

    Finalizare rute proiecte Livrabil 3

--- bugtracker-api/src/controllers/proiectController.js
+++ bugtracker-api/src/controllers/proiectController.js
@@ -1,11 +1,21 @@
 const Proiect = require('../models/Proiect');
 const ListaMembri = require('../models/ListaMembri');
 
+// Functie pentru a obtine toate proiectele (Cea de care ai nevoie pentru afisare)
+exports.obtineToateProiectele = async (req, res) => {
+    try {
+        const proiecte = await Proiect.findAll(); // Citim toate intrarile din tabelul Proiect
+        res.status(200).json(proiecte);
+    } catch (err) {
+        res.status(500).json({ mesaj: 'Eroare la incarcarea proiectelor', eroare: err.message });
+    }
+};
+
 // Functie pentru crearea unui proiect nou (doar pentru MP)
 exports.creeazaProiect = async (req, res) => {
     try {
         const { nume, url } = req.body;
-        
+
         // Cream proiectul in baza de date
         const proiectNou = await Proiect.create({
             nume,
--- bugtracker-api/src/routes/proiectRoutes.js
+++ bugtracker-api/src/routes/proiectRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const proiectController = require('../controllers/proiectController');
 const autentificare = require('../middleware/autentificare');
 
+router.get('/', autentificare, proiectController.obtineToateProiectele);
+
 // Ruta pentru crearea unui proiect (protejata de login)
 router.post('/creare', autentificare, proiectController.creeazaProiect);
 
